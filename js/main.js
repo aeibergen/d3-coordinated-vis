9321b366c3ec34df237b3838cf5a2d084e21bf6f
aeibergen <aeibergen@wisc.edu>
2017-04-02 18:15:37 -0500

    Joined data and created local variables instead of global

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 //March 8, 2017
 //didn't use grat or background -styling choice
 
+(function(){
+
+//pseudo-global variables
+var attrArray = ["Fruit", "Vegetables", "Exercise", "NoExercise", "Overweight", "Obese", "FarmersMarkets", "SNAP"];
+var expressed = attrArray[0]; //initial attribute
+
 //begin script when window loads
 window.onload = setMap();
 
@@ -34,13 +40,49 @@ function setMap(){
         .await(callback);
 
     function callback(error, csvData, states){
-        //translate states TopoJSON
-        console.log(states);
-        var unitedStates = topojson.feature(states, states.objects.states).features;
-        //examine the results
-        console.log(unitedStates);
 
-        //add states to map
+        //translate US topojson
+        var unitedStates = topojson.feature(states, states.objects.states).features;
+
+        //join csv data to GeoJSON enumeration units
+        unitedStates = joinData(unitedStates, csvData);
+
+        //add enumeration units to the map
+        setEnumerationUnits(unitedStates, map, path);
+    };
+}; //goodbye setmap
+
+function joinData(unitedStates, csvData){
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.name; //the CSV primary key
+
+            //loop through geojson regions to find correct region
+            for (var a=0; a<unitedStates.length; a++){
+
+                var geojsonProps = unitedStates[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.name; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr){
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                        console.log(geojsonProps);
+                    });
+                };
+            };
+        };
+
+    return unitedStates;
+};
+
+function setEnumerationUnits(unitedStates, map, path){
+    //add states to map
         var states = map.selectAll(".states")
             .data(unitedStates)
             .enter()
@@ -49,5 +91,6 @@ function setMap(){
                 return "states " + d.properties.name;
             })
             .attr("d", path);
-    };
 };
+
+})();
